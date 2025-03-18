15016a5897cd2b068129a4f42ee0e50751e7494f
Jaren Glenn <jarenglenn@gmail.com>
2025-03-18 01:45:25 -0400

    feat: first nix test

--- flake.nix
+++ flake.nix
@@ -1,17 +1,23 @@
 
 {
-  description = "Home Manager configuration";
+  description = "Home Manager Flake";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
-    homeConfigurations."derethil" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      modules = [ ./home.nix ];
+    defaultPackage.x86_64-linux = home-manager.packages.x86_64-linux;
+
+    homeConfigurations = {
+      "derethil" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+        modules = [ ./home.nix ];
+      };
     };
   };
 }
