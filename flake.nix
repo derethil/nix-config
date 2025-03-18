c1b7369b93779e5cc4b294fce8eec2c9ddb967db
Jaren Glenn <jarenglenn@gmail.com>
2025-03-18 15:32:43 -0400

    feat(flake): use nix-systems

--- flake.nix
+++ flake.nix
@@ -1,22 +1,41 @@
 
 {
-  description = "Home Manager Flake";
+  description = "My Home Manager Configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    systems.url = "github:nix-systems/default-linux";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
-    defaultPackage.x86_64-linux = home-manager.packages.x86_64-linux;
+  outputs = { self, nixpkgs, home-manager, systems, ... }@inputs: let
+    inherit (self) outputs;
+    lib = nixpkgs.lib // home-manager.lib;
+    forEachSystem = f: lib.genAttrs (import systems) (system: f pkgsFor.${system});
+    pkgsFor = lib.genAttrs (import systems) (
+      system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        }
+    );
+  in {
+    inherit lib;
+
+    defaultPackage.x86_64-linux = home-manager.packages.x86_64-linux.home-manager;
 
     homeConfigurations = {
-      "derethil" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
+      "derethil@artemis" = lib.homeManagerConfiguration {
+        pkgs = pkgsFor.x86_64-linux;
         modules = [ ./home.nix ];
+        extraSpecialArgs = {
+          inherit inputs outputs;
+        };
       };
     };
   };
