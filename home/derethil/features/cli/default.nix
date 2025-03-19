059ccc70853859af6e4802d8d1ea73f73a95bcaa
Jaren Glenn <jarenglenn@gmail.com>
2025-03-19 00:32:43 -0400

    feat: install various cli utilities

--- home/derethil/features/cli/default.nix
+++ home/derethil/features/cli/default.nix
@@ -3,9 +3,28 @@
   ];
 
   home.packages = with pkgs; [
+    bat # Better cat
+    bottom # System monitor
+    diff-so-fancy # Better diff
+    duf # Disk Usage
+    ncdu
+    eza # Better ls
+    fd # Better find
+    git-open # Open git repo in browser
+    httpie # Better curl
     jira-cli-go # Jira CLI
+    jq # JSON processor
+    ripgrep # Better grep
+    scc # Project statistics
+    tldr # Command help
+    thefuck # I fucked up
+    zoxide # Better cd
 
-    alejandra # Nix Formatter
+    # Nix Tools
+    alejandra # Formatter
     nixfmt-rfc-style
+    nvd # Differ
+    nix-diff # More detailed differ
+    nh # Nix command wrapper
   ];
 }
