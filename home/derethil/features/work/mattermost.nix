4a0faba898f4abd23d284e200db37addc39540a4
Jaren Glenn <jarenglenn@gmail.com>
2025-04-17 11:08:32 -0400

    fix(work): fix mattermost's desktop file

--- home/derethil/features/work/mattermost.nix
+++ home/derethil/features/work/mattermost.nix
@@ -1,9 +1,16 @@
 {pkgs, ...}: let
-  mattermost-desktop = pkgs.writeShellScriptBin "mattermost-desktop" ''
-    env ELECTRON_PLATFORM_OZONE_HINT=x11 env QT_QPA_PLATFORM=xcb ${pkgs.mattermost-desktop}/bin/mattermost-desktop --ozone-platform=x11 "$@"
-  '';
+  mattermost-desktop-x11 = pkgs.mattermost-desktop.overrideAttrs (oldAttrs: {
+    postFixup =
+      (oldAttrs.postFixup or "")
+      + ''
+        wrapProgram $out/bin/mattermost-desktop \
+          --add-flags "--ozone-platform=x11" \
+          --set ELECTRON_PLATFORM_OZONE_HINT x11 \
+          --set QT_QPA_PLATFORM xcb
+      '';
+  });
 in {
   home.packages = [
-    mattermost-desktop
+    mattermost-desktop-x11
   ];
 }
