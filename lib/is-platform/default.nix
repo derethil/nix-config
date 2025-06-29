cf10aeae01bbf049445512061366f8cc4e80aa29
Jaren Glenn <jarenglenn@gmail.com>
2025-06-29 17:28:26 -0600

    feat(lib): is-platform functions

---
+++ lib/is-platform/default.nix
@@ -0,0 +1,11 @@
+{lib, ...}: let
+  osRelease =
+    if builtins.pathExists "/etc/os-release"
+    then builtins.readFile "/etc/os-release"
+    else "";
+in {
+  platform = {
+    isNixOS = builtins.pathExists /etc/NIXOS;
+    isArch = lib.strings.hasInfix "ID=arch" osRelease;
+  };
+}
