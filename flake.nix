74d746c5a84a806ea3cd078c496ce8421b929efb
Jaren Glenn <jarenglenn@gmail.com>
2025-07-04 23:19:59 -0600

    feat: enable nix community cache

--- flake.nix
+++ flake.nix
@@ -68,5 +68,14 @@
       allowUnfree = true;
       allowUnfreePredicate = _: true;
     };
+    nix.settings = {
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   });
 }
