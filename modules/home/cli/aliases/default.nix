eed22dec8441bc614878c10e7928684c0689f634
Jaren Glenn <jarenglenn@gmail.com>
2025-07-10 16:47:53 -0600

    feat(modules/fish): add c -> clear alias

--- modules/home/cli/aliases/default.nix
+++ modules/home/cli/aliases/default.nix
@@ -20,6 +20,7 @@ with internal; {
         agsv2 = "ags run --directory ~/.config/astal";
         wget = "wget --hsts-file=$XDG_DATA_HOME/wget-hsts";
         hueadm = "hueadm --config ~/.config/.hueadm.json";
+        c = "clear";
       };
       abbreviations = {
         be = "bundle exec";
