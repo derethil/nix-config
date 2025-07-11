12d5a128a3dee269123088a551bea4420b804d50
Jaren Glenn <jarenglenn@gmail.com>
2025-07-10 20:29:01 -0600

    fix(overlays): fix flake-inputs overlay

--- overlays/flake-inputs/default.nix
+++ overlays/flake-inputs/default.nix
@@ -4,13 +4,17 @@
       _: flake: let
         legacyPackages = (flake.legacyPackages or {}).${final.system} or {};
         packages = (flake.packages or {}).${final.system} or {};
-        defaultPackage = (flake.defaultPackage or {}).${final.system} or {};
+        defaultPackage = (flake.defaultPackage or {}).${final.system} or null;
       in
+        # If it's a big package set like nixpkgs, return the full set
         if legacyPackages != {}
-        then legacyPackages // packages # Merge packages into legacyPackages
-        else if defaultPackage != {}
-        then defaultPackage
-        else packages
+        then legacyPackages // packages
+        # Otherwise, return the single default package directly
+        else if defaultPackage != null
+        then defaultPackage # Old schema
+        else if packages ? default
+        then packages.default # New schema
+        else packages # Fallback to package set if no default
     )
     inputs;
 }
