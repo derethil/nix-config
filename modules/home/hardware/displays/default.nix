8c62905cb507d2ce3cd04516836bdd5008986ae6
Jaren Glenn <jarenglenn@gmail.com>
2025-07-11 16:01:24 -0600

    feat(modules/displays): create displays configuration module

---
+++ modules/home/hardware/displays/default.nix
@@ -0,0 +1,25 @@
+{lib, ...}:
+with lib;
+with internal; let
+  defaultPosition = {
+    x = 0;
+    y = 0;
+  };
+
+  display = types.submodule {
+    options = with types; {
+      name = mkOpt str "Monitor" "Human-readable name for the monitor";
+      port = mkOpt str null "Monitor port/connector name (e.g., DP-1)";
+      resolution = mkOpt str "1920x1080" "Monitor resolution in format WIDTHxHEIGHT";
+      framerate = mkOpt int 60 "Monitor refresh rate in Hz";
+      position = mkOpt (attrsOf int) defaultPosition "Monitor position coordinates";
+      scale = mkOpt float 1.0 "Scaling factor for the monitor";
+      enabled = mkBoolOpt true "Whether the monitor is enabled";
+      rotation = mkOpt (enum ["0" "90" "180" "270"]) "0" "Monitor rotation";
+      primary = mkBoolOpt false "Whether this is the primary monitor";
+      vrr = mkOpt int 1 "Variable refresh rate setting for the monitor";
+    };
+  };
+in {
+  options.hardware.displays = mkOpt (types.listOf display) [] "List of monitors to configure";
+}
