7f2cd1d9f593eb7ba971f3b0f5c5ab18c076f94d
Jaren Glenn <jarenglenn@gmail.com>
2025-07-24 03:13:10 -0600

    chore: update nvim-config

--- modules/home/apps/gdlauncher/default.nix
+++ modules/home/apps/gdlauncher/default.nix
@@ -7,12 +7,17 @@
 with lib;
 with internal; let
   cfg = config.apps.gdlauncher;
+
+  pkg =
+    if config.tools.nixgl.enable or false
+    then config.lib.nixGL.wrap pkgs.gdlauncher-carbon
+    else pkgs.gdlauncher-carbon;
 in {
   options.apps.gdlauncher = {
     enable = mkBoolOpt false "Whether to enable GDLauncher-Carbon";
   };
 
   config.home.packages = with pkgs; [
-    (mkIf cfg.enable gdlauncher-carbon)
+    (mkIf cfg.enable pkg)
   ];
 }
