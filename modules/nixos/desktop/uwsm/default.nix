a3791f8262e070dcd54116e7abf7da93b8815e56
Jaren Glenn <jarenglenn@gmail.com>
2025-07-29 11:49:09 -0600

    fix(modules/uwsm): conditional hyprland config

--- modules/nixos/desktop/uwsm/default.nix
+++ modules/nixos/desktop/uwsm/default.nix
@@ -7,6 +7,7 @@
 with lib;
 with internal; let
   cfg = config.desktop.uwsm;
+  nixgl = config.tools.nixgl;
 in {
   options.desktop.uwsm = with types; {
     enable = mkBoolOpt false "Whether to enable uwsm (universal wayland session manager).";
@@ -16,10 +17,13 @@ in {
     programs.uwsm = {
       enable = true;
       waylandCompositors = {
-        hyprland = {
+        hyprland = mkIf config.desktop.hyprland.enable {
           prettyName = "Hyprland";
           comment = "Hyprland compositor";
-          binPath = "${pkgs.hyprland}/bin/Hyprland";
+          binPath = mkMerge [
+            (mkIf (!nixgl.enabled) (getExe pkgs.hyprland))
+            (mkIf (nixgl.enabled) (getExe (config.lib.nixGL.wrap pkgs.hyprland)))
+          ];
           desktopNames = ["Hyprland"];
         };
       };
