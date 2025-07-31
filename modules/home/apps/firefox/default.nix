d6095cbade7872c4445aace508cde525b20cea53
Jaren Glenn <jarenglenn@gmail.com>
2025-07-31 10:50:06 -0600

    fix(modules/firefox): iconUpdateURL -> icon per deprecation

--- modules/home/apps/firefox/default.nix
+++ modules/home/apps/firefox/default.nix
@@ -93,7 +93,7 @@ in {
                   template = "https://nixos.wiki/index.php?search={searchTerms}";
                 }
               ];
-              iconUpdateURL = "https://nixos.wiki/favicon.png";
+              icon = "https://nixos.wiki/favicon.png";
               updateInterval = 24 * 60 * 60 * 1000; # every day
               definedAliases = ["@nw"];
             };
