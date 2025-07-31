c9f6853c707c09db7757ded8e8ecf60177467307
Jaren Glenn <derethil@hestia.local>
2025-07-30 19:55:29 -0600

    feat(homes): add macbook home

---
+++ homes/aarch64-darwin/derethil@hestia/default.nix
@@ -0,0 +1,55 @@
+{lib, ...}:
+with lib;
+with internal; {
+  distro = "darwin";
+  user = {
+    location.latitude = 39.7392;
+    location.longitude = -104.9903;
+  };
+  apps = {
+    foot = disabled;
+    alacritty = enabled;
+    firefox = enabled;
+    gtk-icon-browser = disabled;
+    chromium = enabled;
+    discord = enabled;
+    vesktop = disabled;
+    obs = enabled;
+    obsidian = enabled;
+    spotify = enabled;
+    stremio = enabled;
+    insomnia = enabled;
+    mattermost = enabled;
+    gdlauncher = enabled;
+    heroic = disabled;
+    r2modman = disabled;
+  };
+  languages = {
+    golang = enabled;
+  };
+  desktop = {
+    xdg = enabled;
+    glace-shell = disabled;
+  };
+  tools = {
+    manix = enabled;
+    aws-cli = enabled;
+    git = enabled;
+    jira-cli = enabled;
+    hyprprop = enabled;
+    hyprshot = enabled;
+    hyprpicker = enabled;
+    neovim = enabled;
+  };
+  cli = {
+    fish = enabled;
+    trashy = enabled;
+    direnv = enabled;
+    zoxide = enabled;
+    starship = enabled;
+    tmux = enabled;
+    misc = enabled;
+  };
+
+  home.stateVersion = "25.05";
+}
--- modules/home/distros/default.nix
+++ modules/home/distros/default.nix
@@ -2,7 +2,7 @@
 with lib;
 with internal;
 with types; let
-  distros = enum ["arch" "nixos"];
+  distros = enum ["arch" "nixos" "darwin"];
 in {
   options.distro = mkOpt distros "nixos" "Option to enable distro-specific configurations.";
 }
