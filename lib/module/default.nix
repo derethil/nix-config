3e12b1b1878c482d80b3fb4f39f2c204bc27224f
Jaren Glenn <jarenglenn@gmail.com>
2025-07-31 01:04:30 -0600

    feat(lib): add mkSubmoduleOpt function

--- lib/module/default.nix
+++ lib/module/default.nix
@@ -10,6 +10,22 @@ with lib; rec {
   mkOpt = type: default: description:
     mkOption {inherit type default description;};
 
+  ## Create a submodule option for a NixOS module.
+  ## This function simplifies the creation of submodule options by combining the `mkOpt'` function
+  ## with the `types.submodule` type. It accepts a table of submodule options and creates a
+  ## corresponding option with a `null` default value.
+  ##
+  ## Example usage:
+  ## ```nix
+  ## lib.mkSubmoduleOpt {
+  ##   option1 = mkBoolOpt true "Enable option1.";
+  ##   option2 = mkOpt types.str "default" "Description of option2.";
+  ## }
+  ## ```
+  ##
+  ## @Options -> Any
+  mkSubmoduleOpt = options: mkOpt' (types.submodule {inherit options;}) null;
+
   ## Create a NixOS module option without a description.
   ##
   ## ```nix
