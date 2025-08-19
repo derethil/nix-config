b54e8c91d1c16ea06e55378bc85c1a6648071292
Jaren Glenn <jarenglenn@gmail.com>
2025-08-19 13:07:26 -0600

    fix(modules/cli): remove deprecated --enable_gpu btm alias

--- modules/home/cli/misc/default.nix
+++ modules/home/cli/misc/default.nix
@@ -17,7 +17,6 @@ in {
       l = "eza -la --icons --group-directories-first --time-style=relative";
       lt = "eza --tree --icons --group-directories-first --level=3";
       cat = "bat";
-      btm = "btm --enable_gpu";
       udb = "sudo updatedb";
     };
 
