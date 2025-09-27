a40a7f34b6fc92e54a591e02e73e799dbf44633b
Jaren Glenn <jarenglenn@gmail.com>
2025-09-27 08:47:13 +0000

    feat(systems): initial system config for athena

---
+++ systems/x86_64-linux/athena/default.nix
@@ -0,0 +1,36 @@
+{
+  pkgs,
+  lib,
+  ...
+}: let
+  inherit (lib.internal) enabled;
+in {
+  imports = [
+    ./hardware.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "athena";
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "US/Denver";
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  users.users.derethil = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "networkmanager"];
+  };
+
+  services.openssh.enable = true;
+
+  system.stateVersion = "25.11";
+}
