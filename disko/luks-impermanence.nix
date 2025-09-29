27de7a2e1e3835d4b013328f2036fc1e1270eb06
Jaren Glenn <jarenglenn@gmail.com>
2025-09-28 20:23:55 -0600

    feat(disko): add disko config file for future installations

---
+++ disko/luks-impermanence.nix
@@ -0,0 +1,73 @@
+let
+  device = "/dev/sda";
+  bootSize = "2G";
+  swapSize = "32G";
+  isSSD = true;
+in {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = device;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = bootSize;
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            swap = {
+              size = swapSize;
+              content = {
+                type = "swap";
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "enc";
+                passwordFile = "/tmp/secret.key"; # Interactive password
+                settings = {
+                  allowDiscards = isSSD;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/root" = {
+                      mountpoint = "/";
+                      mountOptions = ["subvol=root" "compress=zstd" "noatime"];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = ["subvol=home" "compress=zstd" "noatime"];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["subvol=nix" "compress=zstd" "noatime"];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = ["subvol=persist" "compress=zstd" "noatime"];
+                    };
+                    "/log" = {
+                      mountpoint = "/var/log";
+                      mountOptions = ["subvol=log" "compress=zstd" "noatime"];
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
+
