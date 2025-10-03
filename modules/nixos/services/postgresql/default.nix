fd55f52f6de6e9e1f25a8b1111459724e2928259
Jaren Glenn <jarenglenn@gmail.com>
2025-10-03 17:50:42 -0600

    feat(nixos/services): fix szuru by adding postgresql service

---
+++ modules/nixos/services/postgresql/default.nix
@@ -0,0 +1,69 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf types flatten concatMapStringsSep optionalString mkAfter;
+  inherit (lib.glace) mkBoolOpt mkSubmoduleListOpt mkOpt mkNullableOpt;
+  cfg = config.glace.services.postgresql;
+in {
+  options.glace.services.postgresql = with types; {
+    enable = mkBoolOpt false "Whether to enable PostgreSQL server.";
+
+    user = {
+      extraGroups = mkOpt (listOf str) [] "Additional groups to add the postgres user to for reading password files.";
+    };
+
+    databases = mkSubmoduleListOpt "Databases to create." {
+      name = mkOpt str null "Database name.";
+      owner = mkOpt (nullOr str) null "Database owner.";
+      users = mkSubmoduleListOpt "Users for this database." {
+        name = mkOpt str null "Username.";
+        passwordFile = mkNullableOpt str null "The path to a file containing a password that is to be set as the user's PASSWORD field.";
+        extraSettings = mkOpt attrs {} "Extra settings to pass to <option>services.postgresql.ensureUsers.<user></option>.";
+      };
+    };
+  };
+
+  config = let
+    users = flatten (map (db: db.users) cfg.databases);
+  in
+    mkIf cfg.enable {
+      services.postgresql = {
+        enable = true;
+        package = pkgs.postgresql_18;
+        ensureDatabases = map (db: db.name) cfg.databases;
+        ensureUsers = map (user:
+          {
+            inherit (user) name;
+          }
+          // user.extraSettings)
+        users;
+      };
+
+      users.users.postgres.extraGroups = cfg.user.extraGroups;
+
+      # NOTE: Taken from: https://github.com/NixOS/nixpkgs/pull/326306
+      systemd.services.postgresql.postStart = mkAfter (
+        concatMapStringsSep "\n" (
+          user: let
+            setPassword = pkgs.writeText "set-pw.sql" ''
+              DO $$
+              DECLARE password TEXT;
+              BEGIN
+                password := trim(both from replace(pg_read_file('${user.passwordFile}'), E'\n', '''));
+                EXECUTE 'ALTER ROLE "${user.name}" WITH PASSWORD' || quote_literal(password);
+              END $$;
+            '';
+          in
+            optionalString (user.passwordFile != null) "${pkgs.postgresql_18}/bin/psql -tAf ${setPassword}"
+        )
+        users
+      );
+
+      glace.system.impermanence.extraDirectories = [
+        "/var/lib/postgresql/${config.services.postgresql.package.psqlSchema}"
+      ];
+    };
+}
--- modules/nixos/services/szuru/default.nix
+++ modules/nixos/services/szuru/default.nix
@@ -4,31 +4,99 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
-  inherit (lib.glace) mkBoolOpt;
+  inherit (lib) mkIf types;
+  inherit (lib.glace) mkBoolOpt mkOpt;
   cfg = config.glace.services.szuru;
 in {
   options.glace.services.szuru = {
     enable = mkBoolOpt false "Enable Szurubooru";
+    port = mkOpt types.port 9000 "Port for the web interface";
   };
 
-  config = mkIf cfg.enable {
-    services.szurubooru = {
-      enable = true;
-      dataDir = "/var/lib/szurubooru";
-      server.settings = {
-        domain = "szuru.local";
-        delete_source_files = "true";
-        port = 9000;
-      };
+  config = let
+    szuruSecretPath = "nixos/services/szurubooru";
+
+    dataDir = "/var/lib/szurubooru";
+
+    user = "szurubooru";
+    group = "szurubooru";
+
+    secret = {
+      inherit group;
+      owner = user;
+      mode = "0440";
     };
+  in
+    mkIf cfg.enable {
+      glace.services.postgresql = {
+        enable = true;
+        user.extraGroups = [group];
+        databases = [
+          {
+            name = "szurubooru";
+            owner = user;
+            users = [
+              {
+                name = user;
+                passwordFile = config.sops.secrets."${szuruSecretPath}/database_password".path;
+                extraSettings.ensureDBOwnership = true;
+              }
+            ];
+          }
+        ];
+      };
+      secrets = {
+        "${szuruSecretPath}/secret" = secret;
+        "${szuruSecretPath}/database_password" = secret;
+      };
 
-    environment.systemPackages = with pkgs; [
-      szurubooru.client
-    ];
+      services.szurubooru = {
+        enable = true;
+        inherit user group dataDir;
+        server.settings = {
+          domain = "http://localhost:9000";
+          delete_source_files = "yes";
+          secretFile = config.sops.secrets."${szuruSecretPath}/secret".path;
+        };
+        database = {
+          passwordFile = config.sops.secrets."${szuruSecretPath}/database_password".path;
+        };
+        client = {
+          package = pkgs.szurubooru.client;
+        };
+      };
 
-    glace.system.impermanence.extraDirectories = [
-      "/var/lib/szurubooru"
-    ];
-  };
+      services.nginx = {
+        enable = true;
+        virtualHosts."szuru.local" = {
+          listen = [
+            {
+              addr = "0.0.0.0";
+              port = cfg.port;
+            }
+          ];
+          locations = {
+            "~ ^/api$" = {
+              return = "302 /api/";
+            };
+            "~ ^/api/(.*)$" = {
+              extraConfig = ''
+                if ($request_uri ~* "/api/(.*)") {
+                  proxy_pass http://127.0.0.1:8080/$1;
+                }
+              '';
+            };
+            "/data/" = {
+              alias = "${dataDir}/data/";
+            };
+            "/" = {
+              tryFiles = "$uri /index.htm";
+              root = "${pkgs.szurubooru.client}";
+            };
+          };
+        };
+      };
+
+      glace.system.impermanence.extraDirectories = [dataDir];
+    };
 }
