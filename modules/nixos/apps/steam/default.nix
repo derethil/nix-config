0214af2bc69c585775ecbb2dc7e9b3fcffd4a311
Jaren Glenn <jarenglenn@gmail.com>
2025-11-20 21:33:33 -0700

    feat(nixos/apps): enable high priority in gamescope

--- modules/nixos/apps/steam/default.nix
+++ modules/nixos/apps/steam/default.nix
@@ -31,6 +31,23 @@ in {
       platformOptimizations.enable = true;
     };
 
+    programs.gamescope = {
+      enable = true;
+      capSysNice = false; # See https://github.com/NixOS/nixpkgs/issues/351516
+    };
+
+    services.ananicy = {
+      enable = true;
+      package = pkgs.ananicy-cpp;
+      rulesProvider = pkgs.ananicy-cpp;
+      extraRules = [
+        {
+          "name" = "gamescope";
+          "nice" = -20;
+        }
+      ];
+    };
+
     programs.gamemode.enable = true;
   };
 }
