fec864aacc712f230fdde5ef800a4f5ebae41e35
Jaren Glenn <jarenglenn@gmail.com>
2025-11-26 11:53:43 -0700

    feat(apps/prismlauncher): create static path for glfw

--- modules/home/apps/gaming/prismlauncher/default.nix
+++ modules/home/apps/gaming/prismlauncher/default.nix
@@ -13,8 +13,11 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      prismlauncher
-    ];
+    home.packages = [pkgs.prismlauncher];
+
+    # NOTE: This is needed to use the WayGL mod to force Wayland rendering. Remember to:
+    # - Set up the WayGL config file
+    # - Change earlyWindowControl to false in /config/fml.toml if using Sinytra & Forge
+    home.file.".local/lib/libglfw.so".source = "${pkgs.glfw}/lib/libglfw.so";
   };
 }
