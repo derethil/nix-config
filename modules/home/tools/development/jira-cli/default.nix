535c4d1c9473f430ac829e0cccf825cb735ee504
Jaren Glenn <jarenglenn@gmail.com>
2026-01-02 14:41:02 -0700

    fix(tools/development): add jira token to environment

--- modules/home/tools/development/jira-cli/default.nix
+++ modules/home/tools/development/jira-cli/default.nix
@@ -4,7 +4,7 @@
   config,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf getExe';
   inherit (lib.glace) mkBoolOpt;
   cfg = config.glace.tools.development.jira-cli;
 in {
@@ -16,5 +16,9 @@ in {
     home.packages = with pkgs; [
       jira-cli-go
     ];
+
+    secrets."home/tools/jira_cli/api_token" = {};
+
+    home.sessionVariables.JIRA_API_TOKEN = "$(${getExe' pkgs.coreutils "cat"} ${config.sops.secrets."home/tools/jira_cli/api_token".path})";
   };
 }
