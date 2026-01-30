0f5ad20173be714ce81834d20f44d4abf8659d22
Jaren Glenn <jarenglenn@gmail.com>
2026-01-29 22:03:32 -0700

    feat(services/openssh): set ssh keys in nix

--- modules/home/services/openssh/default.nix
+++ modules/home/services/openssh/default.nix
@@ -6,12 +6,30 @@
   inherit (lib) mkIf;
   inherit (lib.glace) mkBoolOpt;
   cfg = config.glace.services.openssh;
+  userName = config.home.username;
 in {
   options.glace.services.openssh = {
     enable = mkBoolOpt false "Whether to enable OpenSSH client configuration.";
   };
 
   config = mkIf cfg.enable {
+    secrets."users/${userName}/ssh/private_key" = {};
+    secrets."users/${userName}/ssh/public_key" = {};
+
+    sops.templates."ssh-private-key" = {
+      name = "id_ed25519";
+      content = config.sops.placeholder."users/${userName}/ssh/private_key";
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      mode = "0600";
+    };
+
+    sops.templates."ssh-public-key" = {
+      name = "id_ed25519.pub";
+      content = config.sops.placeholder."users/${userName}/ssh/public_key";
+      path = "${config.home.homeDirectory}/.ssh/id_ed25519.pub";
+      mode = "0644";
+    };
+
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
