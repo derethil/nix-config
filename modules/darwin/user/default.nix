a0a4e83c52f1ac7d23e17d164bf575463486617a
Jaren Glenn <jarenglenn@gmail.com>
2026-02-05 11:08:05 -0700

    refactor(user): remove duplicate options from darwin/nixos modules

--- modules/darwin/user/default.nix
+++ modules/darwin/user/default.nix
@@ -3,21 +3,10 @@
   config,
   ...
 }: let
-  inherit (lib) types mkIf;
-  inherit (lib.glace) mkOpt mkBoolOpt;
+  inherit (lib) mkIf;
 
   cfg = config.glace.user;
 in {
-  options.glace.user = with types; {
-    name = mkOpt str "derethil" "The name to use for the user account.";
-    fullName = mkOpt str "Jaren Glenn" "The full name of the user.";
-    email = mkOpt str "jarenglenn@gmail.com" "The email of the user.";
-    uid = mkOpt (nullOr int) 501 "UID of the user.";
-    extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
-    superuser = mkBoolOpt true "Whether the user is a superuser.";
-    home = mkOpt str "/Users/${cfg.name}" "Home directory of the user";
-  };
-
   config = {
     users.users.${cfg.name} = {
       inherit (cfg) name home;
--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -3,24 +3,12 @@
   config,
   ...
 }: let
-  inherit (lib) types optional;
-  inherit (lib.glace) mkOpt mkBoolOpt;
+  inherit (lib) optional;
   cfg = config.glace.user;
+  password = "users/${cfg.name}/hashedPassword";
 in {
-  options.glace.user = with types; {
-    name = mkOpt str "derethil" "The name to use for the user account.";
-    fullName = mkOpt str "Jaren Glenn" "The full name of the user.";
-    email = mkOpt str "jarenglenn@gmail.com" "The email of the user.";
-    uid = mkOpt int 1000 "UID of the user.";
-    extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
-    superuser = mkBoolOpt true "Whether the user is a superuser.";
-    home = mkOpt str "/home/${cfg.name}" "Home directory of the user";
-  };
-
-  config = let
-    passwordPath = "users/${cfg.name}/hashedPassword";
-  in {
-    secrets.${passwordPath} = {
+  config = {
+    secrets.${password} = {
       neededForUsers = true;
     };
 
@@ -29,7 +17,7 @@ in {
       users.${cfg.name} = {
         inherit (cfg) name uid home;
         isNormalUser = true;
-        hashedPasswordFile = config.sops.secrets.${passwordPath}.path;
+        hashedPasswordFile = config.sops.secrets.${password}.path;
         group = "users";
         extraGroups = cfg.extraGroups ++ (optional cfg.superuser "wheel");
       };
