0fbbadb5f1084383577ea62be94e95fc604ca979
Jaren Glenn <jarenglenn@gmail.com>
2026-02-05 11:33:22 -0700

    fix(sops): use user age key in home manager

--- modules/home/tools/sops/default.nix
+++ modules/home/tools/sops/default.nix
@@ -1,9 +1,6 @@
-{pkgs, ...}: {
+{config, pkgs, ...}: {
   config = {
     home.packages = with pkgs; [sops age];
-    sops.age = {
-      keyFile = "/persist/etc/sops/age/keys.txt";
-      sshKeyPaths = ["/persist/etc/ssh/ssh_host_ed25519_key"];
-    };
+    sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
   };
 }
