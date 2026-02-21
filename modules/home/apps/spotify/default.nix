cd8e95447050d2572b238bdbdda9cca51a5a582f
Jaren Glenn <jarenglenn@gmail.com>
2026-02-21 11:26:45 -0700

    fix(apps/spotify): disable auto update in darwin

--- modules/home/apps/spotify/default.nix
+++ modules/home/apps/spotify/default.nix
@@ -4,17 +4,31 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkMerge;
   inherit (lib.glace) mkBoolOpt;
+  inherit (pkgs.stdenv) hostPlatform;
   cfg = config.glace.apps.spotify;
 in {
   options.glace.apps.spotify = {
     enable = mkBoolOpt false "Whether to enable Spotify";
   };
 
-  config = mkIf cfg.enable {
-    home.packages = [
-      pkgs.spotify
-    ];
-  };
+  config = mkIf cfg.enable (mkMerge [
+    {
+      home.packages = [
+        pkgs.spotify
+      ];
+    }
+
+    (mkIf hostPlatform.isDarwin {
+      home.activation.disableSpotifyUpdates = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        SPOTIFY_UPDATE_DIR=~/Library/Application\ Support/Spotify/PersistentCache/Update
+        if ! /usr/bin/stat -f "%Sf" "$SPOTIFY_UPDATE_DIR" 2> /dev/null | grep -q uchg; then
+          rm -rf "$SPOTIFY_UPDATE_DIR"
+          mkdir -p "$SPOTIFY_UPDATE_DIR"
+          /usr/bin/chflags uchg "$SPOTIFY_UPDATE_DIR"
+        fi
+      '';
+    })
+  ]);
 }
