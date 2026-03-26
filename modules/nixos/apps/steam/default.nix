8537c32dbf70c47505acdfb4920b9d2f1cf5fd42
Jaren Glenn <jarenglenn@gmail.com>
2026-03-26 11:21:50 -0600

    feat(apps/steam): use cachyOS ananicy optimizations

--- modules/nixos/apps/steam/default.nix
+++ modules/nixos/apps/steam/default.nix
@@ -4,12 +4,13 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
-  inherit (lib.glace) mkBoolOpt;
+  inherit (lib) mkIf types;
+  inherit (lib.glace) mkBoolOpt mkOpt;
   cfg = config.glace.apps.steam;
 in {
   options.glace.apps.steam = {
     enable = mkBoolOpt false "Enable Steam with Proton support";
+    cachyosOptimizations = mkBoolOpt true "Whether to enable CachyOS optimizations for Steam and steam games.";
   };
 
   config = mkIf cfg.enable {
@@ -31,26 +32,24 @@ in {
       platformOptimizations.enable = true;
     };
 
-    programs.gamescope = {
-      enable = true;
-      capSysNice = false; # See https://github.com/NixOS/nixpkgs/issues/351516
-    };
-
     environment.systemPackages = [
       pkgs.dotnetCorePackages.sdk_8_0-bin
       pkgs.fna3d
     ];
 
-    services.ananicy = {
+    programs.gamescope = {
+      enable = true;
+      capSysNice = false;
+    };
+
+    # work around for issue with capSysNice not working in gamescope. even though it still
+    # complains that it doesn't have cap nice ability to set it its own nice value.  ananicy
+    # is setting it -20 (highest priority).
+    # See: https://github.com/NixOS/nixpkgs/issues/351516
+    services.ananicy = mkIf cfg.cachyosOptimizations {
       enable = true;
       package = pkgs.ananicy-cpp;
-      rulesProvider = pkgs.ananicy-cpp;
-      extraRules = [
-        {
-          "name" = "gamescope";
-          "nice" = -20;
-        }
-      ];
+      rulesProvider = pkgs.ananicy-rules-cachyos;
     };
 
     programs.gamemode.enable = true;
