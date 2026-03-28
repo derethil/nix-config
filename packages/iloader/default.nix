63075477af30be95e3dd3ea89cacfc3688162364
Jaren Glenn <jarenglenn@gmail.com>
2026-03-27 19:17:25 -0600

    chore(packages): update iloader to latest

--- packages/iloader/default.nix
+++ packages/iloader/default.nix
@@ -1,28 +1,29 @@
 {
   lib,
+  pkgs,
   stdenv,
   fetchurl,
   appimageTools,
-  undmg,
   ...
-}:
-let
+}: let
   pname = "iloader";
-  version = "1.1.6";
+  version = "2.0.10";
 
-  sources = {
+  sources = rec {
     x86_64-linux = {
       url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-linux-amd64.AppImage";
-      hash = "sha256-L1fFwFjdIrrhviBlwORhSDXsNYgrT1NcVKAKlss6h4o=";
+      hash = "sha256-ENhpG8My9AJ3JQbhb77K+95RGB7LodYXmnzZzWorNyQ=";
     };
+    aarch64-linux = {
+      url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-linux-aarch64.AppImage";
+      hash = "sha256-tyubdzan4bnAbyYmRQ9TY8yWW7xIoJ1BvQHJ6w4gB7s=";
+    };
+
     x86_64-darwin = {
       url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-darwin-universal.app.tar.gz";
-      hash = "sha256-OQa5cQx2KIh1op3wMH0I8v9vXOq6PeF00Z0NQRL823s=";
+      hash = "sha256-aJFr8MUq6d04M4zE/FURZpY1owQ1aXcvAxPdb3syR8g=";
     };
-    aarch64-darwin = {
-      url = "https://github.com/nab138/iloader/releases/download/v${version}/iloader-darwin-universal.app.tar.gz";
-      hash = "sha256-OQa5cQx2KIh1op3wMH0I8v9vXOq6PeF00Z0NQRL823s=";
-    };
+    aarch64-darwin = x86_64-darwin;
   };
 
   src = fetchurl (sources.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}"));
@@ -31,32 +32,66 @@ let
     description = "A user-friendly desktop application for sideloading apps onto iOS devices";
     homepage = "https://github.com/nab138/iloader";
     license = licenses.mit;
-    platforms = ["x86_64-linux" "x86_64-darwin" "aarch64-darwin"];
+    platforms = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
     maintainers = [];
   };
 in
-if stdenv.isLinux then
-  let
-    appimageContents = appimageTools.extractType2 {inherit pname version src;};
-  in
-    appimageTools.wrapType2 {
-      inherit pname version src meta;
+  if stdenv.isLinux
+  then let
+    # Extract the AppImage and remove bundled Wayland libraries that conflict with system libraries.
+    # The AppImage bundles incompatible Wayland libraries that cause EGL_BAD_PARAMETER errors.
+    # By removing them, the application will use system Wayland libraries instead.
+    # See: https://github.com/nab138/iloader/issues/77
+    extracted = appimageTools.extract {
+      inherit pname version src;
+      postExtract = ''
+        chmod -R +w $out
+        rm -f $out/usr/lib/*wayland*so*
+        rm -f $out/usr/lib/im-wayland*.so
+      '';
+    };
 
-      extraInstallCommands = ''
-        install -Dm444 ${appimageContents}/iloader.desktop -t $out/share/applications
-        install -Dm444 ${appimageContents}/iloader.png -t $out/share/pixmaps
+    fhsEnv = pkgs.buildFHSEnv {
+      name = "${pname}-fhs";
+      # Include default AppImage dependencies plus system Wayland libraries
+      targetPkgs = pkgs:
+        (appimageTools.defaultFhsEnvArgs.targetPkgs pkgs)
+        ++ (with pkgs; [
+          wayland
+          libxkbcommon
+        ]);
+      multiPkgs = appimageTools.defaultFhsEnvArgs.multiPkgs;
+      runScript = "${extracted}/AppRun";
+    };
+  in
+    stdenv.mkDerivation {
+      inherit pname version meta;
+
+      dontUnpack = true;
+
+      installPhase = ''
+        mkdir -p $out/bin $out/share/applications $out/share/pixmaps
+
+        cat > $out/bin/${pname} << EOF
+        #!/bin/sh
+        exec ${fhsEnv}/bin/${pname}-fhs "\$@"
+        EOF
+        chmod +x $out/bin/${pname}
+
+        install -Dm444 ${extracted}/iloader.desktop -t $out/share/applications
+        install -Dm444 ${extracted}/iloader.png -t $out/share/pixmaps
       '';
     }
-else
-  stdenv.mkDerivation {
-    inherit pname version src meta;
+  else
+    stdenv.mkDerivation {
+      inherit pname version src meta;
 
-    sourceRoot = ".";
+      sourceRoot = ".";
 
-    installPhase = ''
-      runHook preInstall
-      mkdir -p $out/Applications
-      cp -r iloader.app $out/Applications/
-      runHook postInstall
-    '';
-  }
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out/Applications
+        cp -r iloader.app $out/Applications/
+        runHook postInstall
+      '';
+    }
