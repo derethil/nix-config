5f0647ad71f1f05d1b23197fcbede0eb64b2d1c2
Jaren Glenn <jarenglenn@gmail.com>
2026-05-05 13:54:40 -0600

    fix(apps/prismlauncher): wrap in FHS environment with system libraries
    This allows mods like MCEF to work properly that provide their own fork
    of system libraries that cannot be run on NixOS since they are a
    dynamically linked executable.

--- modules/home/apps/gaming/prismlauncher/default.nix
+++ modules/home/apps/gaming/prismlauncher/default.nix
@@ -4,16 +4,79 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf getExe;
+  inherit (pkgs.stdenv.hostPlatform) isDarwin;
   inherit (lib.glace) mkBoolOpt;
+
   cfg = config.glace.apps.gaming.prismlauncher;
+
+  prismlauncher-fhs = pkgs.buildFHSEnv {
+    name = "prismlauncher-fhs";
+
+    targetPkgs = pkgs:
+      with pkgs; [
+        # Package
+        prismlauncher
+
+        # For watermedia (embedded VLC)
+        libvlc
+
+        # For MCEF (embedded Chromium)
+        alsa-lib
+        atk
+        at-spi2-atk
+        cairo
+        cups
+        dbus
+        expat
+        glib
+        gtk3
+        libdrm
+        libgbm
+        libxkbcommon
+        mesa
+        nspr
+        nss
+        pango
+        xorg.libX11
+        xorg.libXcomposite
+        xorg.libXdamage
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXi
+        xorg.libXrandr
+        xorg.libXrender
+        xorg.libXScrnSaver
+        xorg.libXtst
+        xorg.libxcb
+        xorg.libxshmfence
+      ];
+
+    runScript = "prismlauncher";
+  };
+
+  primlauncher-wrapped = pkgs.symlinkJoin {
+    name = "prismlauncher";
+    paths = [pkgs.prismlauncher prismlauncher-fhs];
+    nativeBuildInputs = [pkgs.makeWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/prismlauncher --run 'exec ${getExe prismlauncher-fhs}'
+    '';
+  };
 in {
   options.glace.apps.gaming.prismlauncher = {
     enable = mkBoolOpt false "Whether to enable Prism Launcher.";
   };
 
   config = mkIf cfg.enable {
-    home.packages = [pkgs.prismlauncher];
+    home.packages = [
+      (
+        # MacOS can run dynamically linked libraries just fine
+        if isDarwin
+        then pkgs.prismlauncher
+        else primlauncher-wrapped
+      )
+    ];
 
     # NOTE: This is needed to use the WayGL mod to force Wayland rendering. Remember to:
     # - Set up the WayGL config file
