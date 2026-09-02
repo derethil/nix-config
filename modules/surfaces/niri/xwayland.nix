2464952ce733d775fb0528f7eb3fd9232afe44b6
Jaren Glenn <jarenglenn@pm.me>
2026-09-02 12:10:19 -0600

    fix(xwayland): pin to 0.8.1 to fix steam popups

--- modules/surfaces/niri/xwayland.nix
+++ modules/surfaces/niri/xwayland.nix
@@ -1,10 +1,23 @@
 {
-  flake.modules.homeManager.niri = {
-    lib,
-    pkgs,
-    ...
-  }: {
-    wayland.windowManager.niri.settings.xwayland-satellite.path =
-      lib.getExe pkgs.unstable.xwayland-satellite;
+  flake.modules.homeManager.niri = {pkgs, ...}: let
+    # 0.8.2 (3273a0f) breaks override-redirect popups on niri; Steam menus
+    # close instantly. https://github.com/Supreeeme/xwayland-satellite/issues/468
+    xwayland-satellite = pkgs.xwayland-satellite.overrideAttrs (_: rec {
+      cargoDeps = pkgs.rustPlatform.importCargoLock {
+        allowBuiltinFetchGit = true;
+        lockFile = "${src}/Cargo.lock";
+      };
+
+      src = pkgs.fetchFromGitHub {
+        hash = "sha256-BUE41HjLIGPjq3U8VXPjf8asH8GaMI7FYdgrIHKFMXA=";
+        owner = "Supreeeme";
+        repo = "xwayland-satellite";
+        rev = "v${version}";
+      };
+
+      version = "0.8.1";
+    });
+  in {
+    home.packages = [xwayland-satellite];
   };
 }
